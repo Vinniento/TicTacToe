c05d96a7ffe660571b8ae253b8cc92e8114645f0
Vincent Schauer <schauervincent@gmail.com>
2020-03-10 09:22:51 +0100

    bilder + music + win richtig

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,9 +2,9 @@ package com.example.tictactoe
 
 import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,8 +15,11 @@ class MainActivity : AppCompatActivity() {
     val currSymbol = { if (count % 2 == 0) "X" else "O" }
     var setStatusText = { statustext.text = "Spieler ${currSymbol()} ist dran" }
     var originalColor: Drawable? = null
-
+    //so spart man sich die null checks
+    //lateinit var  mp: MediaPlayer
+    var mp: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,32 +36,39 @@ class MainActivity : AppCompatActivity() {
             field7,
             field8
         )
-        
+
         allFields.forEach { field ->
             field.setOnClickListener {
-                if (field.text.isEmpty()) {
-                    field.text = currSymbol().capitalize()
-                    if (field.text == "X")
-                        field.setBackgroundColor(Color.GREEN)
-                    else
-                        field.setBackgroundColor(Color.BLUE)
-
-                    if (hasWon()) {
-                        Toast.makeText(
-                            this,
-                            "winner is player ${currSymbol()} after ${count + 1} rounds",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        allFields.forEach { field -> field.isClickable = false }
-                    }
-                    count++
-                    setStatusText()
-                }
+                clickOnField(field, allFields)
             }
         }
 
         reset(allFields)
 
+    }
+
+    fun clickOnField(field: Button, allFields: ArrayList<Button>) {
+        if (field.text.isEmpty()) {
+            field.text = currSymbol().capitalize()
+            if (field.text == "X")
+                field.setBackgroundResource(R.drawable.pusheen_knots)
+            else
+                field.setBackgroundResource(R.drawable.pusheen_crosses)
+
+            if (hasWon()) {
+                Toast.makeText(
+                    this,
+                    "winner is player ${currSymbol()} after ${count + 1} rounds",
+                    Toast.LENGTH_LONG
+                ).show()
+                allFields.forEach { field -> field.isClickable = false }
+                mp = MediaPlayer.create(this, R.raw.firework_finish )
+                mp?.start()
+            } else if (allFields.all { field.text != "" })
+                Toast.makeText(this, "Unentschieden", Toast.LENGTH_LONG)
+            count++
+            setStatusText()
+        }
     }
 
     fun reset(allFields: ArrayList<Button>) {
@@ -70,8 +80,10 @@ class MainActivity : AppCompatActivity() {
                 field.isClickable = true
                 field.background = originalColor
             }
+            mp?.reset()
 
         }
+
     }
 
     fun checkIfThreeSame(f0: Button, f1: Button, f2: Button): Boolean {
@@ -81,13 +93,13 @@ class MainActivity : AppCompatActivity() {
 
     fun hasWon(): Boolean {
         //horiz
-        return (checkIfThreeSame(field0, field1, field3) ||
-                checkIfThreeSame(field2, field4, field5) ||
+        return (checkIfThreeSame(field0, field1, field2) ||
+                checkIfThreeSame(field3, field4, field5) ||
                 checkIfThreeSame(field6, field7, field8) ||
                 //vert
-                checkIfThreeSame(field0, field2, field6) ||
+                checkIfThreeSame(field0, field3, field6) ||
                 checkIfThreeSame(field1, field4, field7) ||
-                checkIfThreeSame(field3, field5, field8) ||
+                checkIfThreeSame(field2, field5, field8) ||
                 //diag
                 checkIfThreeSame(field0, field4, field8) ||
                 checkIfThreeSame(field2, field4, field6))
