297edd079e994c2e012b7ce05848a125e816149a
Vincent Schauer <schauervincent@gmail.com>
2020-03-31 16:32:29 +0200

    splash screen hinzugefügt

---
+++ app/src/main/java/com/example/tictactoe/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tictactoe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        startActivity(Intent(this, MainActivity::class.java))
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
